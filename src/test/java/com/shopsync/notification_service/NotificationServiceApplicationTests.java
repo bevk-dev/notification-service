f69a09bd38fec328d857de67aaa969d185616884
UBevk <bevk.urh@gmail.com>
2026-01-13 09:12:53 +0100

    .

--- src/test/java/com/shopsync/notification_service/NotificationServiceApplicationTests.java
+++ src/test/java/com/shopsync/notification_service/NotificationServiceApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 class NotificationServiceApplicationTests {
 
 	@Test
